15d0ea8a276be10d87a53b2175cdff470fb9505b
vincent <vin.beck24@gmail.com>
2026-06-30 22:13:36 +0200

    added new language selector it has currently no function

--- app/src/commonMain/kotlin/de/sirvincelot/juganaut/ui/compose/settings/GeneralSettings.kt
+++ app/src/commonMain/kotlin/de/sirvincelot/juganaut/ui/compose/settings/GeneralSettings.kt
@@ -6,18 +6,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.sirvincelot.juganaut.ui.compose.states.GeneralConfigHolder
 import de.sirvincelot.juganaut.ui.theme.AppTheme
 import de.sirvincelot.juganaut.ui.theme.ThemeMode
@@ -49,6 +56,7 @@ fun GeneralSettings() {
     ) {
         SettingsHeadline(stringResource(Res.string.general))
 //        DarkModeSetting(generalData)
+        LanguageSetting()
     }
 }
 
@@ -82,4 +90,40 @@ fun SingleChoiceSegmentedButton(modifier: Modifier = Modifier, generalData: Gene
             )
         }
     }
+}
+
+@Composable
+fun LanguageSetting() {
+    val langs = listOf("English", "Deutsch")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(langs[0]) }
+
+
+
+    Column(Modifier.selectableGroup()) {
+        langs.forEach { text ->
+            Row(
+                Modifier
+               .fillMaxWidth()
+               .selectable(
+                   selected = (text == selectedOption),
+                   onClick = {onOptionSelected(text)},
+                   role = Role.RadioButton
+               )
+               .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null
+                )
+                Text(
+                    text,
+                    modifier = Modifier.padding(start = 16.dp),
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+
+    }
+
 }
